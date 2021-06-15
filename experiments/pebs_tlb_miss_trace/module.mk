071e7d40c72138a39b7909df1a5f6b03703f848e
Mohammad Agbarya <e.mohammad@gmail.com>
2021-06-15 12:59:51 +0300

    Fix issue #3

--- experiments/pebs_tlb_miss_trace/module.mk
+++ experiments/pebs_tlb_miss_trace/module.mk
@@ -2,7 +2,9 @@ MODULE_NAME := experiments/pebs_tlb_miss_trace
 SUBMODULES :=
 
 PERF_RECORD_FREQUENCY := $$(( 2**15 ))
-PERF_MEM_STLB_MISSES_EVENTS := mem_uops_retired.stlb_miss_loads:p,mem_uops_retired.stlb_miss_stores:p
+STLB_MISS_LOADS_PEBS_EVENT := $(shell perf list | grep retired | grep mem | grep stlb_miss_loads | tr -d ' ')
+STLB_MISS_STORES_PEBS_EVENT := $(shell perf list | grep retired | grep mem | grep stlb_miss_stores | tr -d ' ')
+PERF_MEM_STLB_MISSES_EVENTS := $(STLB_MISS_LOADS_PEBS_EVENT):p,$(STLB_MISS_STORES_PEBS_EVENT):p
 PERF_MEM_RECORD_CMD := perf record --data --count=$(PERF_RECORD_FREQUENCY) --event=$(PERF_MEM_STLB_MISSES_EVENTS)
 
 PEBS_TLB_MISS_TRACE_OUTPUT := $(MODULE_NAME)/perf.data
