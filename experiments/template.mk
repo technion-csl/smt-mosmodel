14afe06e7cfc5a306066d804fb7592a5cfe8bdfe
Idan Yaniv <idanyani@cs.technion.ac.il>
2021-06-23 14:25:40 +0300

    Clean the layouts file when requested

--- experiments/template.mk
+++ experiments/template.mk
@@ -46,7 +46,7 @@ $(RESULTS): results/%/mean.csv: experiments/%
 	$(COLLECT_RESULTS_SCRIPT) --experiments_root=$< --repeats=$(NUM_OF_REPEATS) \
 		--layouts=$(LAYOUT_LIST) --output_dir=$(dir $@)
 
-DELETED_TARGETS := $(EXPERIMENT_REPEATS) $(EXPERIMENTS)
+DELETED_TARGETS := $(EXPERIMENTS) $(EXPERIMENT_REPEATS) $(LAYOUTS_FILE)
 CLEAN_TARGETS := $(addsuffix /clean,$(DELETED_TARGETS))
 $(CLEAN_TARGETS): %/clean: %/delete
 $(MODULE_NAME)/clean: $(CLEAN_TARGETS)
