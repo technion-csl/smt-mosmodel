929e2f5830576f876de59f32562963fc47cd1703
Mohammad Agbarya <e.mohammad@gmail.com>
2021-06-24 23:34:18 +0300

    Update setCpuMemoryAffinity script to move the bounded cpu offline and then online before start running the experiment

--- scripts/setCpuMemoryAffinity.sh
+++ scripts/setCpuMemoryAffinity.sh
@@ -9,12 +9,18 @@ node_number="$1"
 shift
 command="$@"
 
-bound_cpu_cores=$(cat /sys/devices/system/node/node${node_number}/cpulist)
+bound_cpu_core=$(cat /sys/devices/system/node/node${node_number}/cpulist | rev | cut -d ',' -f 1 | cut -d '-' -f 1 | rev)
 echo "Binding the process to memory node: $node_number,"
-echo "and its local CPU cores: $bound_cpu_cores"
-taskset_command="taskset --cpu-list $bound_cpu_cores"
+echo "and its local CPU core: $bound_cpu_core"
+taskset_command="taskset --cpu-list $bound_cpu_core"
 numactl_command="numactl --membind $node_number"
 submit_command="$taskset_command $numactl_command"
+
+echo "Move the following cores to online-->offline: $bound_cpu_core"
+sudo bash -c 'echo 0 > /sys/devices/system/cpu/cpu${bound_cpu_core}/online'
+sleep 1
+sudo bash -c 'echo 1 > /sys/devices/system/cpu/cpu${bound_cpu_core}/online'
+sleep 1
 
 $submit_command $command
 
