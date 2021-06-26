70b1646037239ee4d4fa63351b6d840926978e67
Mohammad Agbarya <e.mohammad@gmail.com>
2021-06-27 01:11:44 +0300

    Add plotMemoryLayout.py script

---
+++ scripts/plotMemoryLayout.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python3
+
+page_size = (2**21)
+
+import pandas as pd
+def getLayoutHugepages(layout_file):
+    df = pd.read_csv(layout_file)
+    df = df[df['type'] == 'brk']
+    df = df[df['pageSize'] == 2097152]
+    pages = []
+    for index, row in df.iterrows():
+        start_page = int(row['startOffset'] / page_size)
+        end_page = int(row['endOffset'] / page_size)
+        for i in range(start_page, end_page, 1):
+            pages.append(i)
+    return pages
+
+def binary_data(data, yshift=0, limit=-1):
+    data.sort()
+    if limit == -1:
+        limit = data[-1]
+    return [yshift+1 if x in data else yshift for x in range(limit)]
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--layout_files', required=True)
+parser.add_argument('-l', '--layout_labels', required=True)
+parser.add_argument('-m', '--memory_footprint', default='memory_footprint.txt')
+parser.add_argument('-o', '--output',  default='all_points', required=True)
+args = parser.parse_args()
+
+import math
+# read memory-footprints
+footprint_df = pd.read_csv(args.memory_footprint)
+brk_footprint = footprint_df['brk-max'][0]
+brk_footprint = math.ceil(brk_footprint / page_size) * page_size
+
+import numpy as np
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(9,2.5))
+# set x, y labels
+plt.xlabel('page number (2MB)')
+plt.ylabel('page type')
+
+limit = math.ceil(brk_footprint / page_size)
+x = np.arange(0, limit)
+# read the layout files
+layout_files = args.layout_files.strip().split(',')
+layout_labels = args.layout_labels.strip().split(',')
+num_layouts = len(layout_files)
+for i in range(num_layouts - 1, -1, -1):
+    f = layout_files[i]
+    l = layout_labels[i]
+    pages = getLayoutHugepages(f)
+    shift = i * 2
+    bindata = binary_data(pages, shift, limit)
+    y = np.array(bindata)
+    #ax.step(x, y, label=l)
+    ax.plot(x, y, '|', label=l)
+
+handles, labels = ax.get_legend_handles_labels()
+#ax.legend(handles, labels, loc='best')
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
+        ncol=2, fancybox=True, shadow=False)
+ax.set_yticks(np.arange((num_layouts)*2))
+ax.set_yticklabels(['4KB', '2MB'] * num_layouts)
+
+fig.savefig(args.output, bbox_inches='tight')
+
