3be9e5ff17b18d6e5aae14969823e5cb3fb5e822
Idan Yaniv <idanyani@cs.technion.ac.il>
2021-08-19 13:39:10 +0300

    Defer the expansion of some makefile variables (because perf may not be installed yet)

--- experiments/pebs_tlb_miss_trace/module.mk
+++ experiments/pebs_tlb_miss_trace/module.mk
@@ -2,10 +2,10 @@ MODULE_NAME := experiments/pebs_tlb_miss_trace
 SUBMODULES :=
 
 PERF_RECORD_FREQUENCY := $$(( 2**15 ))
-STLB_MISS_LOADS_PEBS_EVENT := $(shell perf list | grep retired | grep mem | grep stlb_miss_loads | tr -d ' ')
-STLB_MISS_STORES_PEBS_EVENT := $(shell perf list | grep retired | grep mem | grep stlb_miss_stores | tr -d ' ')
-PERF_MEM_STLB_MISSES_EVENTS := $(STLB_MISS_LOADS_PEBS_EVENT):p,$(STLB_MISS_STORES_PEBS_EVENT):p
-PERF_MEM_RECORD_CMD := perf record --data --count=$(PERF_RECORD_FREQUENCY) --event=$(PERF_MEM_STLB_MISSES_EVENTS)
+STLB_MISS_LOADS_PEBS_EVENT = $(shell perf list | grep retired | grep mem | grep stlb_miss_loads | tr -d ' ')
+STLB_MISS_STORES_PEBS_EVENT = $(shell perf list | grep retired | grep mem | grep stlb_miss_stores | tr -d ' ')
+PERF_MEM_STLB_MISSES_EVENTS = $(STLB_MISS_LOADS_PEBS_EVENT):p,$(STLB_MISS_STORES_PEBS_EVENT):p
+PERF_MEM_RECORD_CMD = perf record --data --count=$(PERF_RECORD_FREQUENCY) --event=$(PERF_MEM_STLB_MISSES_EVENTS)
 
 PEBS_EXP_OUT_DIR := $(MODULE_NAME)/repeat0
 PEBS_TLB_MISS_TRACE_OUTPUT := $(PEBS_EXP_OUT_DIR)/perf.data
