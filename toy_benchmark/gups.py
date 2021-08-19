742cee630e7aa3f2b8a0581a9a6f88fedbf365e9
Idan Yaniv <idanyani@cs.technion.ac.il>
2021-08-19 13:17:52 +0300

    Remove the numpy import from the gups toy benchmark

--- toy_benchmark/gups.py
+++ toy_benchmark/gups.py
@@ -6,9 +6,8 @@ parser.add_argument('-l', '--log2_length', default=24, help='log2 of the array l
 parser.add_argument('-r', '--repeats', default=1, help='number of repeats')
 args = parser.parse_args()
 
-import numpy as np
 array_length = 2**(args.log2_length)
-array = np.arange(array_length)
+array = range(array_length)
 # the array memory footprint is typically 2**(args.log2_length+3) because sizeof(int)==8
 
 array_sum = 0
