82bbd29c21148edf1dd4a20b0d13848d3e6d8396
Idan Yaniv <idanyani@cs.technion.ac.il>
2021-08-23 16:06:14 +0300

    Remove the old experiments that use 1GB random windows

--- analysis/module.mk
+++ analysis/module.mk
@@ -5,12 +5,8 @@ SUBMODULES := \
 	sliding_window \
 	linear_models_coeffs \
 	pebs_tlb_miss_trace \
-	random_window_1g \
 	mosmodel \
-	all_data #\
-	train_mosmodel test_mosmodel all_data \
-	tlb_misses_vs_table_walks \
-	single_page_size model_errors perf_mem
+	all_data
 SUBMODULES := $(addprefix $(MODULE_NAME)/,$(SUBMODULES))
 
 ARRANGE_DATA_TO_PLOT_SCRIPT := $(MODULE_NAME)/arrangeDataToPlot.py
