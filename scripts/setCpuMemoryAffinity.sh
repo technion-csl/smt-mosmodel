6c95501dfc8fcff83c56c43eaa2fc298dcfc5adb
Mohammad Agbarya <e.mohammad@gmail.com>
2021-08-28 22:59:45 +0300

    Bind the benchmark to an isolated cpu if exist, otherwise to the last cpu in the bounded memory node

--- scripts/setCpuMemoryAffinity.sh
+++ scripts/setCpuMemoryAffinity.sh
@@ -9,12 +9,26 @@ node_number="$1"
 shift
 command="$@"
 
-bound_cpu_cores=$(cat /sys/devices/system/node/node${node_number}/cpulist)
+bound_cpu_core=$(cat /sys/devices/system/node/node${node_number}/cpulist | rev | cut -d ',' -f 1 | cut -d '-' -f 1 | rev)
 echo "Binding the process to memory node: $node_number,"
-echo "and its local CPU cores: $bound_cpu_cores"
-taskset_command="taskset --cpu-list $bound_cpu_cores"
+echo "and its local CPU core: $bound_cpu_core"
+taskset_command="taskset --cpu-list $bound_cpu_core"
 numactl_command="numactl --membind $node_number"
 submit_command="$taskset_command $numactl_command"
+
+isolated_cpus_list=$(cat /sys/devices/system/cpu/isolated)
+isolated_cpus=$(echo {1..100} | { cut -d" " -f"${isolated_cpus_list// /,}"; })
+if `echo ${isolated_cpus} | grep -w -q ${bound_cpu_core}`; then
+    echo "cpu $bound_cpu_core is isolated"
+    echo "Move the following cores to online-->offline: $bound_cpu_core"
+    sudo bash -c "echo 0 > /sys/devices/system/cpu/cpu${bound_cpu_core}/online"
+    sleep 1
+    sudo bash -c "echo 1 > /sys/devices/system/cpu/cpu${bound_cpu_core}/online"
+    sleep 1
+else
+    echo "$isolated_cpus_list does not contain cpu number: $bound_cpu_core"
+    echo "skipping moving the bounded core to offline->online..."
+fi
 
 $submit_command $command
 
