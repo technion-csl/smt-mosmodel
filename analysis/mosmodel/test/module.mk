2d63a837fbcf567982f18b142af0bc12f012fba5
Mohammad Agbarya <e.mohammad@gmail.com>
2021-10-24 11:47:31 +0300

    Remove subgroups experiments from train set

--- analysis/mosmodel/test/module.mk
+++ analysis/mosmodel/test/module.mk
@@ -1,6 +1,6 @@
 MODULE_NAME := analysis/mosmodel/test
 SUBMODULES := 
 
-MODEL_EXPERIMENTS := random_window_2m subgroups_windows
+MODEL_EXPERIMENTS := random_window_2m
 
 include $(MOSMODEL_TEMPLATE_MAKEFILE)
