715135b35df92b266ee416758d144f72d485b820
Mohammad Agbarya <e.mohammad@gmail.com>
2021-11-18 14:52:24 +0200

    Create auto-mosalloc layouts such that the generated layout will be pased on the previous one as much as possible

--- experiments/auto_mosalloc/createLayouts.py
+++ experiments/auto_mosalloc/createLayouts.py
@@ -38,17 +38,61 @@ def writeLayout(num_layout, windows, output):
                 end_offset=(w+1) * page_size)
     configuration.exportToCSV(output, 'layout'+str(num_layout))
 
-def findTlbCoverageWindows(df, tlb_coverage_percentage):
+def findTlbCoverageWindows(df, tlb_coverage_percentage, prev_windows):
     epsilon = 0.5
+    windows = None
+    while windows == None:
+        windows = _findTlbCoverageWindows(df, tlb_coverage_percentage, prev_windows, epsilon)
+        epsilon += 0.5
+    return windows
+
+'''
+import itertools
+def _findTlbCoverageWindows(df, tlb_coverage_percentage, prev_windows, epsilon):
+    # iterate over all subsets of prev_windows starting from the whole set
+    # to find hugepages partitioning that contains a maximal common subset
+    # of the previos layout
+    for subset_size in range(len(prev_windows),-1,-1):
+        for subset in list(itertools.combinations(prev_windows, subset_size)):
+            windows = _findTlbCoverageWindowsBasedOnSubset(df, tlb_coverage_percentage, list(subset), epsilon)
+            if windows:
+                return windows
+'''
+
+def _findTlbCoverageWindows(df, tlb_coverage_percentage, prev_windows, epsilon):
+    # based on the fact that selected pages in prev_windows are ordered
+    # from heaviest to the lightest
+    for i in range(len(prev_windows)+1):
+        windows = _findTlbCoverageWindowsBasedOnSubset(df, tlb_coverage_percentage, prev_windows[i:], epsilon)
+        if windows:
+            return windows
+
+def _findTlbCoverageWindowsBasedOnSubset(df, tlb_coverage_percentage, base_windows, epsilon):
+    total_weight = sum(base_windows)
+    # use a new list instead of using the existing base_windows list to
+    # keep it sorted according to page weights
     windows = []
-    total_weight = 0
     for index, row in df.iterrows():
         weight = row['NUM_ACCESSES']
         page_number = row['PAGE_NUMBER']
+        if page_number in base_windows:
+            # pages from base_windows already included in the total weight
+            # just add them without increasing the total weight
+            windows.append(page_number)
+            continue
         if (total_weight + weight) <= (tlb_coverage_percentage + epsilon):
             #print('page: {page} - weight: {weight}'.format(page=page_number, weight=weight))
             total_weight += weight
             windows.append(page_number)
+        if total_weight >= (tlb_coverage_percentage - epsilon):
+            break
+
+    if total_weight > (tlb_coverage_percentage + epsilon) \
+            or total_weight < (tlb_coverage_percentage - epsilon):
+        return []
+    # add tailed pages from base_windows that were not selected (because
+    # we are already reached the goal weight)
+    windows += list(set(base_windows) - set(windows))
     return windows
 
 # read mem-bins
@@ -65,10 +109,11 @@ total_access = df['NUM_ACCESSES'].sum()
 df['NUM_ACCESSES'] = df['NUM_ACCESSES'].mul(100).divide(total_access)
 df = df.sort_values('NUM_ACCESSES', ascending=False)
 
+windows = []
 # build layouts
 for num_layout in range(args.num_layouts):
     tlb_coverage_percentage = step_size * (num_layout+1)
-    windows = findTlbCoverageWindows(df, tlb_coverage_percentage)
+    windows = findTlbCoverageWindows(df, tlb_coverage_percentage, windows)
     print('TLB-coverage = {coverage} - Paegs = {pages}'.format(coverage=tlb_coverage_percentage, pages=windows))
     writeLayout(num_layout+1, windows, args.output)
 
