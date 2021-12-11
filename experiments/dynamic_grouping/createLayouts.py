edb90b61966897c0d12e6176a78d820d7e6300b2
Mohammad Agbarya <e.mohammad@gmail.com>
2021-12-11 22:22:44 +0200

    Update dynamic_groups layout generator to create the basic groups as a continiuos hugepages

--- experiments/dynamic_grouping/createLayouts.py
+++ experiments/dynamic_grouping/createLayouts.py
@@ -36,9 +36,97 @@ def writeLayout(layout, windows, output):
                 end_offset=(w+1) * page_size)
     configuration.exportToCSV(output, layout)
 
+def calculateTlbCoverage(pebs_df, pages):
+    total_weight = pebs_df.query(
+            'PAGE_NUMBER in {pages}'.format(pages=pages))\
+                    ['NUM_ACCESSES'].sum()
+    return total_weight
+
+def buildGroupsSequentially(orig_pebs_df, layouts_dir):
+    pebs_df = orig_pebs_df.copy()
+    threshold = 60
+    pebs_df.sort_values('NUM_ACCESSES', ascending=False, inplace=True)
+    groups = []
+    all_pages = []
+    i = 0
+    epsilon = 1
+    desired_weights = [30, 20, 10]
+    for index, row in pebs_df.iterrows():
+        current_total_weight = 0
+        current_group = []
+        
+        page_number = int(row['PAGE_NUMBER'])
+        if page_number in all_pages:
+            continue
+        
+        weight = row['NUM_ACCESSES']
+        if weight > (desired_weights[i] + epsilon):
+            continue
+        
+        min_page = max_page = page_number
+        for k in range(25):
+            page = min_page - 1
+            if page < 0 or page in all_pages:
+                break
+            min_page -= 1
+        for k in range(25):
+            page = max_page + 1
+            if page > last_page or page in all_pages:
+                break
+            max_page += 1
+        query = pebs_df.query(
+            'PAGE_NUMBER <= {max_page} and PAGE_NUMBER >= {min_page} and PAGE_NUMBER not in {all_pages}'.format(
+                max_page=max_page,
+                min_page=min_page,
+                all_pages=all_pages))
+        if query['NUM_ACCESSES'].sum() < desired_weights[i]:
+            continue
+        query_df = query.sort_values('PAGE_NUMBER', ascending=True)
+        page_index = query_df[query_df['PAGE_NUMBER'] == page_number].index.to_list()[0]
+        left_index = page_index-1 if page_index > 0 else 0
+        right_index = page_index+1 if page_index < len(query_df)-1 else page_index
+        
+        current_total_weight += weight
+        current_group.append(page_number)
+        
+        while right_index < len(query_df) or left_index >= 0:
+            # if we already achieved our goal then stop
+            if current_total_weight > (desired_weights[i] - epsilon):
+                break
+            # if there is a right side then get its weight
+            right_page = query_df.iat[right_index, 0]
+            if right_index != page_index and right_index < len(query_df):
+                right_weight = query_df.iat[right_index, 1]
+            else:
+                right_weight = -1
+            # if there is a left side then get its weight
+            left_page = query_df.iat[left_index, 0]
+            if left_index != page_index and left_index >= 0:
+                left_weight = query_df.iat[left_index, 1]
+            else:
+                left_weight = -1
+            # if we are adding too much then stop
+            if (current_total_weight + right_weight) > (desired_weights[i] + epsilon) \
+            or (current_total_weight + left_weight) > (desired_weights[i] + epsilon):
+                break
+            # take the larger side
+            if right_weight > left_weight:
+                current_total_weight += right_weight
+                current_group.append(right_page)
+                right_index += 1
+            else:
+                current_total_weight += left_weight
+                current_group.append(left_page)
+                left_index -= 1
+        groups.append(list(set(current_group)))
+        all_pages += current_group
+        i += 1
+        if i == len(desired_weights):
+            break
+    return groups
+
 def buildGroups(pebs_df, layouts_dir):
     threshold = 60
-    total_weight = 0
     pebs_df.sort_values('NUM_ACCESSES', ascending=False, inplace=True)
     groups = []
     current_group = []
@@ -73,6 +161,7 @@ import pandas as pd
 footprint_pebs_df = pd.read_csv(args.memory_footprint)
 mmap_footprint = footprint_pebs_df['anon-mmap-max'][0]
 brk_footprint = footprint_pebs_df['brk-max'][0]
+last_page = int(brk_footprint / 4096)
 
 # read mem-bins
 pebs_df = pd.read_csv(args.pebs_mem_bins, delimiter=',')
@@ -92,7 +181,7 @@ pebs_df = pebs_df.sort_values('NUM_ACCESSES', ascending=False)
 import itertools
 if args.layout == 'layout1':
     i = 1
-    groups = buildGroups(pebs_df, args.layouts_dir)
+    groups = buildGroupsSequentially(pebs_df, args.layouts_dir)
     for subset_size in range(len(groups)+1):
         for subset in itertools.combinations(groups, subset_size):
             windows = []
@@ -102,6 +191,7 @@ if args.layout == 'layout1':
             i += 1
             print(layout_name)
             print('#hugepages: '+ str(len(windows)))
+            print('weight: ' + str(calculateTlbCoverage(pebs_df, windows)))
             print('hugepages: ' + str(windows))
             print('---------------')
             writeLayout(layout_name, windows, args.layouts_dir)
