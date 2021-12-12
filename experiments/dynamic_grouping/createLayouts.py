a40b00003b73036de84cab356363e4bd9b63a2bd
Mohammad Agbarya <e.mohammad@gmail.com>
2021-12-12 21:04:04 +0200

    Update dynamic_grouping layouts generator to adapt the groups desired-wieghts to find three groups

--- experiments/dynamic_grouping/createLayouts.py
+++ experiments/dynamic_grouping/createLayouts.py
@@ -42,72 +42,67 @@ def calculateTlbCoverage(pebs_df, pages):
                     ['NUM_ACCESSES'].sum()
     return total_weight
 
-def buildGroupsSequentially(orig_pebs_df, layouts_dir):
+def buildGroupsSequentially(orig_pebs_df, layouts_dir, desired_weights):
     pebs_df = orig_pebs_df[['PAGE_NUMBER', 'NUM_ACCESSES']]
     threshold = 60
-    pebs_df.sort_values('NUM_ACCESSES', ascending=False, inplace=True)
+    pebs_df = pebs_df.sort_values('NUM_ACCESSES', ascending=False)
     groups = []
     all_pages = []
     i = 0
     epsilon = 1
-    desired_weights = [30, 20, 10]
     for index, row in pebs_df.iterrows():
         current_total_weight = 0
         current_group = []
-        
+
         page_number = int(row['PAGE_NUMBER'])
         if page_number in all_pages:
             continue
-        
+
         weight = row['NUM_ACCESSES']
         if weight > (desired_weights[i] + epsilon):
             continue
-        
-        min_page = max_page = page_number
-        for k in range(25):
-            page = min_page - 1
-            if page < 0 or page in all_pages:
-                break
-            min_page -= 1
-        for k in range(25):
-            page = max_page + 1
-            if page > last_page or page in all_pages:
-                break
-            max_page += 1
+
         query = pebs_df.query(
             'PAGE_NUMBER <= {max_page} and PAGE_NUMBER >= {min_page} and PAGE_NUMBER not in {all_pages}'.format(
-                max_page=max_page,
-                min_page=min_page,
+                max_page=page_number+25,
+                min_page=page_number-25,
                 all_pages=all_pages))
         if query['NUM_ACCESSES'].sum() < desired_weights[i]:
             continue
-        query_df = query.sort_values('PAGE_NUMBER', ascending=True)
+        query_df = query.sort_values('PAGE_NUMBER', ascending=True).reset_index()
+        query_df = query_df[['PAGE_NUMBER', 'NUM_ACCESSES']]
+
         page_index = query_df[query_df['PAGE_NUMBER'] == page_number].index.to_list()[0]
         left_index = page_index-1 if page_index > 0 else 0
         right_index = page_index+1 if page_index < len(query_df)-1 else page_index
-        
+
         current_total_weight += weight
         current_group.append(page_number)
-        
+
+        found = True
         while right_index < len(query_df) or left_index >= 0:
             # if we already achieved our goal then stop
             if current_total_weight > (desired_weights[i] - epsilon):
+                found = False
                 break
             # if there is a right side then get its weight
-            right_page = query_df.iat[right_index, 0]
             if right_index != page_index and right_index < len(query_df):
+                right_page = query_df.iat[right_index, 0]
                 right_weight = query_df.iat[right_index, 1]
             else:
+                right_page = -1
                 right_weight = -1
             # if there is a left side then get its weight
-            left_page = query_df.iat[left_index, 0]
             if left_index != page_index and left_index >= 0:
+                left_page = query_df.iat[left_index, 0]
                 left_weight = query_df.iat[left_index, 1]
             else:
+                left_page = -1
                 left_weight = -1
             # if we are adding too much then stop
             if (current_total_weight + right_weight) > (desired_weights[i] + epsilon) \
             or (current_total_weight + left_weight) > (desired_weights[i] + epsilon):
+                found = False
                 break
             # take the larger side
             if right_weight > left_weight:
@@ -118,9 +113,10 @@ def buildGroupsSequentially(orig_pebs_df, layouts_dir):
                 current_total_weight += left_weight
                 current_group.append(left_page)
                 left_index -= 1
-        groups.append(list(set(current_group)))
-        all_pages += current_group
-        i += 1
+        if found:
+            groups.append(list(set(current_group)))
+            all_pages += current_group
+            i += 1
         if i == len(desired_weights):
             break
     return groups
@@ -181,7 +177,11 @@ pebs_df = pebs_df.sort_values('NUM_ACCESSES', ascending=False)
 import itertools
 if args.layout == 'layout1':
     i = 1
-    groups = buildGroupsSequentially(pebs_df, args.layouts_dir)
+    desired_weights = [30, 20, 10]
+    groups = []
+    while len(groups) != 3:
+        groups = buildGroupsSequentially(pebs_df, args.layouts_dir, desired_weights)
+        desired_weights = [0.9*w for w in desired_weights]
     for subset_size in range(len(groups)+1):
         for subset in itertools.combinations(groups, subset_size):
             windows = []
