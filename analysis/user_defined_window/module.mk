3786e6a39bfef7a8ced0f39e596f3cb3a070f154
Mohammad Agbarya <e.mohammad@gmail.com>
2021-12-23 21:53:42 +0200

    Remove obsolete user_defined experiment and analysis files

--- analysis/user_defined_window/module.mk
+++
@@ -1,9 +0,0 @@
-MODULE_NAME := analysis/user_defined_window
-SUBMODULES := 
-
-$(MODULE_NAME)/%: NUM_OF_REPEATS := $(USER_DEFINED_WINDOW_NUM_OF_REPEATS)
-$(MODULE_NAME)/%: CONFIGURATION_LIST := \
-	$(call array_to_comma_separated,$(USER_DEFINED_WINDOW_CONFIGURATIONS))
-
-include $(COMMON_ANALYSIS_MAKEFILE)
-
