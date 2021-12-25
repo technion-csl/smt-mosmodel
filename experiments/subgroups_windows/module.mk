a678aa22468f380302b3b756d86445c5f1c531db
Mohammad Agbarya <e.mohammad@gmail.com>
2021-12-25 22:49:48 +0200

    Update subgroups_windows experiment to create 9 layouts (subgroup) from 3 layouts (the group) that are created from the heaviest pages

--- experiments/subgroups_windows/module.mk
+++ experiments/subgroups_windows/module.mk
@@ -1,6 +1,6 @@
 MODULE_NAME := experiments/subgroups_windows
 
-NUM_LAYOUTS := 33
+NUM_LAYOUTS := 9
 undefine LAYOUTS #allow the template to create new layouts based on the new NUM_LAYOUTS
 
 include $(EXPERIMENTS_TEMPLATE)
@@ -9,9 +9,8 @@ CREATE_SUBGROUPS_WINDOWS_LAYOUTS_SCRIPT := $(MODULE_NAME)/createLayouts.py
 $(LAYOUT_FILES): $(MEMORY_FOOTPRINT_FILE) $(HOT_REGION_FILE)
 	$(CREATE_SUBGROUPS_WINDOWS_LAYOUTS_SCRIPT) \
 		--memory_footprint=$(MEMORY_FOOTPRINT_FILE) \
-		--hot_region=$(HOT_REGION_FILE) \
-		--num_layouts=$(NUM_LAYOUTS) \
-		--output=$(dir $@)/..
+		--pebs_mem_bins=$(MEM_BINS_2MB_CSV_FILE) \
+		--exp_dir=$(dir $@)/..
 
 override undefine NUM_LAYOUTS
 override undefine LAYOUTS
