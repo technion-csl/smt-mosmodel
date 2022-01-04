b043aa3116f275aa950d0eb5c9bf8c03f191da74
Mohammad Agbarya <e.mohammad@gmail.com>
2022-01-04 12:31:29 +0200

    Add median scatter files

--- analysis/common.mk
+++ analysis/common.mk
@@ -4,16 +4,18 @@ $(MODULE_NAME)/%: EXPERIMENTS_ROOT := $(EXPERIMENTS_ROOT)
 ANALYSIS_DIRS := $(MODULE_NAME)
 NORMALIZED_SCATTER_FILE := $(ANALYSIS_DIRS)/normalized_scatter.csv
 SCATTER_FILE := $(ANALYSIS_DIRS)/scatter.csv
+MEDIAN_SCATTER_FILE := $(ANALYSIS_DIRS)/median_scatter.csv
 WHISKER_FILE := $(ANALYSIS_DIRS)/whisker.csv
 ALL_REPEATS_SCATTER_FILE := $(ANALYSIS_DIRS)/all_repeats_scatter.csv
 NORMALIZED_SCATTER_CHART := $(ANALYSIS_DIRS)/normalized_scatter.pdf
 SCATTER_CHART := $(ANALYSIS_DIRS)/scatter.pdf
+MEDIAN_SCATTER_CHART := $(ANALYSIS_DIRS)/median_scatter.pdf
 WHISKER_CHART := $(ANALYSIS_DIRS)/whisker.pdf
 ALL_REPEATS_CHART := $(ANALYSIS_DIRS)/all_repeats.pdf
 POLY_CHART := $(ANALYSIS_DIRS)/poly.pdf
 
-ALL_CHARTS := $(SCATTER_CHART) $(WHISKER_CHART) $(ALL_REPEATS_CHART) $(POLY_CHART)
-ALL_CSVS := $(NORMALIZED_SCATTER_FILE) $(SCATTER_FILE) $(WHISKER_FILE) $(ALL_REPEATS_SCATTER_FILE)
+ALL_CHARTS := $(SCATTER_CHART) $(MEDIAN_SCATTER_CHART) $(WHISKER_CHART) $(ALL_REPEATS_CHART) $(POLY_CHART)
+ALL_CSVS := $(NORMALIZED_SCATTER_FILE) $(SCATTER_FILE) $(MEDIAN_SCATTER_FILE) $(WHISKER_FILE) $(ALL_REPEATS_SCATTER_FILE)
 
 $(MODULE_NAME)%: ALL_CSVS := $(ALL_CSVS) 
 $(MODULE_NAME)%: ALL_CHARTS := $(ALL_CHARTS)
@@ -29,6 +31,9 @@ $(NORMALIZED_SCATTER_CHART): $(NORMALIZED_SCATTER_FILE)
 $(SCATTER_CHART): $(SCATTER_FILE)
 	gnuplot -e "input_file='$<'" -e "output_file='$@'" $(SCATTER_PLOT)
 
+$(MEDIAN_SCATTER_CHART): $(MEDIAN_SCATTER_FILE)
+	gnuplot -e "input_file='$<'" -e "output_file='$@'" $(SCATTER_PLOT)
+
 $(ALL_REPEATS_CHART): $(ALL_REPEATS_SCATTER_FILE)
 	if [[ $(NUM_OF_REPEATS) != 1 ]]; then
 	gnuplot -e "input_file='$<'" -e "output_file='$@'" $(SCATTER_PLOT)
@@ -42,6 +47,11 @@ $(WHISKER_CHART): $(WHISKER_FILE)
 $(NORMALIZED_SCATTER_FILE): analysis/%/normalized_scatter.csv: results/%/mean.csv
 	mkdir -p $(dir $@)
 	$(ARRANGE_DATA_TO_PLOT) --normalize='by-y' --y-metric='cpu-cycles' \
+		--mean_file=$< --output=$@
+
+$(MEDIAN_SCATTER_FILE): analysis/%/median_scatter.csv: results/%/median.csv
+	mkdir -p $(dir $@)
+	$(ARRANGE_DATA_TO_PLOT) --y-metric='cpu-cycles' \
 		--mean_file=$< --output=$@
 
 $(SCATTER_FILE): analysis/%/scatter.csv: results/%/mean.csv
