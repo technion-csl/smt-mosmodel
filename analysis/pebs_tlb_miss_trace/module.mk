7a709d9b332c6fa8ed4b9e785a924d69793ed661
Mohammad Agbarya <e.mohammad@gmail.com>
2022-01-20 21:33:32 +0200

    Add an error message to makefile if some pebs samples were lost

--- analysis/pebs_tlb_miss_trace/module.mk
+++ analysis/pebs_tlb_miss_trace/module.mk
@@ -49,7 +49,9 @@ $(MEM_BINS_4KB_CSV_FILE): $(PEBS_EXP_OUT_DIR)
 		$(FIX_DELIM_IN_PERF_MEM_OUTPUT_HEADER) | \
 		$(BIN_ADDRESSES) --width=4096 --output=$@ \
 		--pools_range_file=$^/pools_base_pointers.out ;} >> $(dir $@)/analyze.log 2>&1
-	! grep -q "lost" $(dir $@)/analyze.log
+	if grep -q "lost" $(dir $@)/analyze.log; then
+		$(error some pebs records were lost, try to decrease sampling rate and re-run)
+	fi
 
 $(MEM_BINS_2MB_BRK_RATIO_CSV_FILE): $(MEM_BINS_2MB_CSV_FILE)
 	$(CALCULATE_PAGES_WEIGHTS) --type brk --input $< --output $@
@@ -59,19 +61,25 @@ $(MEM_BINS_2MB_CSV_FILE): $(PEBS_EXP_OUT_DIR)
 		$(FIX_DELIM_IN_PERF_MEM_OUTPUT_HEADER) | \
 		$(BIN_ADDRESSES) --width=$$(( 2**21 )) --output=$@ \
 		--pools_range_file=$^/pools_base_pointers.out ;} >> $(dir $@)/analyze.log 2>&1
-	! grep -q "lost" $(dir $@)/analyze.log
+	if grep -q "lost" $(dir $@)/analyze.log; then
+		$(error some pebs records were lost, try to decrease sampling rate and re-run)
+	fi
 
 $(MEM_ACCESS_COUNT_FILE): $(PEBS_EXP_OUT_DIR)
 	{ $(PERF_MEM_REPORT_PREFIX) -i $^/perf.data report | \
 		$(FIX_DELIM_IN_PERF_MEM_OUTPUT_HEADER) | \
 		$(COUNT_MEMORY_ACCESSES) -o $@ -p $^/pools_base_pointers.out ;} >> $(dir $@)/analyze.log 2>&1
-	! grep -q "lost" $(dir $@)/analyze.log
+	if grep -q "lost" $(dir $@)/analyze.log; then
+		$(error some pebs records were lost, try to decrease sampling rate and re-run)
+	fi
 
 $(MEM_ACCESSES_FILE): $(PEBS_EXP_OUT_DIR)
 	{ $(PERF_MEM_REPORT_PREFIX) -i $^/perf.data report | \
 		$(FIX_DELIM_IN_PERF_MEM_OUTPUT_HEADER) | \
 		$(PARSE_PERF_MEM_RAW_FILE) -o $@ -p $^/pools_base_pointers.out ;} >> $(dir $@)/analyze.log 2>&1
-	! grep -q "lost" $(dir $@)/analyze.log
+	if grep -q "lost" $(dir $@)/analyze.log; then
+		$(error some pebs records were lost, try to decrease sampling rate and re-run)
+	fi
 
 $(MODULE_NAME)/clean:
 	rm -rf $(PEBS_TARGET_FILES)
