68ba2ce0f8fdab5783a4d3235d6d6682a1f2b9a3
Mohammad Agbarya <e.mohammad@gmail.com>
2022-04-26 13:49:37 +0300

    smart_genetic_scan: Collect single_page_size results before start building layouts

--- experiments/smart_genetic_scan/module.mk
+++ experiments/smart_genetic_scan/module.mk
@@ -7,7 +7,8 @@ undefine LAYOUTS #allow the template to create new layouts based on the new NUM_
 include $(EXPERIMENTS_TEMPLATE)
 
 CREATE_SMART_GENETIC_SCAN_LAYOUTS := $(MODULE_NAME)/createLayouts.py
-$(LAYOUT_FILES): $(SMART_GENETIC_SCAN_EXP_DIR)/layouts/%.csv: $(MEMORY_FOOTPRINT_FILE) $(SINGLE_PAGE_SIZE_EXPERIMENT) analysis/pebs_tlb_miss_trace/mem_bins_2mb.csv
+$(LAYOUT_FILES): $(SMART_GENETIC_SCAN_EXP_DIR)/layouts/%.csv: $(MEMORY_FOOTPRINT_FILE) $(SINGLE_PAGE_SIZE_EXPERIMENT) analysis/pebs_tlb_miss_trace/mem_bins_2mb.csv \
+	results/single_page_size/mean.csv 
 	mkdir -p results/smart_genetic_scan
 	$(COLLECT_RESULTS) --experiments_root=experiments/smart_genetic_scan --repeats=$(NUM_OF_REPEATS) \
 		--output_dir=results/smart_genetic_scan --remove_outliers
